818617d6ba68d34a95e084082e401ab3c0a9bdd3
Matheus <matheusduartegalvao@gmail.com>
2020-09-02 23:42:42 -0300

    Ajustes na aplicacao

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Text,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
 import api from './services/api';
@@ -18,6 +19,17 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Matheus Duarte',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" />
@@ -29,6 +41,13 @@ export default function App() {
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -42,6 +61,20 @@ const styles = StyleSheet.create({
 
   project: {
     color: '#FFF',
-    fontSize: 28,
+    fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
